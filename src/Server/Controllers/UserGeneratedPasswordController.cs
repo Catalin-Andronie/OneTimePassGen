7284823dddfa58a560ab98e24478c94b2551e2c5
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-22 01:11:24 +0300

    Implement 'api/user-generated-passwords' endpoint

---
+++ src/Server/Controllers/UserGeneratedPasswordController.cs
@@ -0,0 +1,97 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OneTimePassGen.Domain.Entities;
+using OneTimePassGen.Infrastructure.Persistance;
+using OneTimePassGen.Server.Models;
+
+namespace OneTimePassGen.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/user-generated-passwords")]
+[Produces("application/json")]
+public sealed class UserGeneratedPasswordController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserGeneratedPasswordController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IList<GeneratedPasswordDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<IList<GeneratedPasswordDto>> GetUserPasswordsAsync(CancellationToken cancellationToken = default)
+    {
+        string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return await _dbContext.UserGeneratedPasswords
+                    .Where(p => p.UserId == currentUserId)
+                    .Select(p => new GeneratedPasswordDto
+                    {
+                        Id = p.Id,
+                        UserId = p.UserId,
+                        Password = p.Password,
+                        ExpiersAt = p.ExpiersAt,
+                        CreatedAt = p.CreatedAt
+                    })
+                    .ToListAsync(cancellationToken)
+                    .ConfigureAwait(false);
+    }
+
+    [HttpGet("{userPasswordId}")]
+    [ProducesResponseType(typeof(IList<GeneratedPasswordDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<GeneratedPasswordDto>> GetUserPasswordAsync(Guid userPasswordId, CancellationToken cancellationToken = default)
+    {
+        string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var generatedPassword = await _dbContext.UserGeneratedPasswords
+                                            .Where(p => p.Id == userPasswordId && p.UserId == currentUserId)
+                                            .Select(p => new GeneratedPasswordDto
+                                            {
+                                                Id = p.Id,
+                                                UserId = p.UserId,
+                                                Password = p.Password,
+                                                ExpiersAt = p.ExpiersAt,
+                                                CreatedAt = p.CreatedAt
+                                            })
+                                            .SingleOrDefaultAsync(cancellationToken)
+                                            .ConfigureAwait(false);
+
+        return generatedPassword ?? (ActionResult<GeneratedPasswordDto>)NotFound();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(GeneratedPasswordDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<GeneratedPasswordDto>> CreateUserPasswordAsync(CancellationToken cancellationToken = default)
+    {
+        string currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        string passwordValue = Guid.NewGuid().ToString();
+        var now = DateTimeOffset.Now;
+        var generatedPassword = new UserGeneratedPassword
+        {
+            Id = Guid.NewGuid(),
+            UserId = currentUserId,
+            Password = passwordValue,
+            CreatedAt = now,
+            ExpiersAt = now.AddSeconds(30)
+        };
+
+        await _dbContext.UserGeneratedPasswords.AddAsync(generatedPassword, cancellationToken).ConfigureAwait(false);
+        await _dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+        return CreatedAtAction(nameof(GetUserPasswordAsync), new { generatedPassword.Id }, generatedPassword);
+    }
+}
---
+++ src/Server/Models/GeneratedPasswordDto.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OneTimePassGen.Server.Models;
+
+/// <summary>
+///     Represents a generated password that a user possesses.
+/// </summary>
+public sealed class GeneratedPasswordDto
+{
+    /// <summary>
+    ///     Gets or sets the identifier for this generated password.
+    /// </summary>
+    [Required]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the primary key of the user that is linked to this generated password.
+    /// </summary>
+    [Required]
+    public string UserId { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the generated password as plain text.
+    /// </summary>
+    [Required]
+    public string Password { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets the date and time, in UTC, when the generated password expiers.
+    /// </summary>
+    /// <remarks>
+    ///     A value in the feature means the generated password is not expierd.
+    /// </remarks>
+    [Required]
+    public DateTimeOffset ExpiersAt { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the date and time, in UTC, when the entry was created.
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
+}
