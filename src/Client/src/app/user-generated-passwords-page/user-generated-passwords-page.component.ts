25c9bf1ea5a445a6ff0fd89c56a59d6322c9fc24
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-23 22:45:04 +0300

    Move http request out of constructor

--- src/Client/src/app/user-generated-passwords-page/user-generated-passwords-page.component.ts
+++ src/Client/src/app/user-generated-passwords-page/user-generated-passwords-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-generated-passwords-page',
   templateUrl: './user-generated-passwords-page.component.html'
 })
-export class UserGeneratedPasswordsPageComponent {
+export class UserGeneratedPasswordsPageComponent implements OnInit {
   public userGeneratedPasswords: UserGeneratedPassword[] = [];
 
-  public constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<UserGeneratedPassword[]>(baseUrl + 'api/user-generated-passwords')
+  public constructor(
+    private _http: HttpClient,
+    @Inject('BASE_URL') private _baseUrl: string)
+  {
+  }
+
+  public ngOnInit(): void {
+    this._http.get<UserGeneratedPassword[]>(this._baseUrl + 'api/user-generated-passwords')
       .subscribe({
         next: (result) => this.userGeneratedPasswords = result,
         error: (e) => console.error(e)
