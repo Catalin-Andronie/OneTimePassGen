8f285907a7e37a30f44369660f9e45a5ef5c2c3f
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-24 18:14:57 +0300

    Remove boilerplate code

--- src/Client/src/app/app.module.ts
+++ src/Client/src/app/app.module.ts
@@ -6,9 +6,6 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -19,9 +16,6 @@ import { UserGeneratedPasswordService } from './services/UserGeneratedPasswordSe
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     UserGeneratedPasswordsPageComponent
   ],
   imports: [
@@ -30,9 +24,7 @@ import { UserGeneratedPasswordService } from './services/UserGeneratedPasswordSe
     FormsModule,
     ApiAuthorizationModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: '', redirectTo: 'generated-passwords', pathMatch: 'full' },
       { path: 'generated-passwords', component: UserGeneratedPasswordsPageComponent, canActivate: [AuthorizeGuard] },
     ])
   ],
