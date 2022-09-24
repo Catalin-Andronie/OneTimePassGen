647b0d5ae0354f810701bd5957c220eeb27f4fe2
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-24 12:54:32 +0300

    Display Yes/No depending if the generated password expired

--- src/Client/src/app/models/UserGeneratedPasswordModel.ts
+++ src/Client/src/app/models/UserGeneratedPasswordModel.ts
@@ -4,4 +4,9 @@ export class UserGeneratedPasswordModel {
   public password!: string;
   public expiersAt!: Date;
   public createdAt!: Date;
+
+  public get isExpired(): boolean {
+    const now = new Date(Date.now());
+    return now > new Date(this.expiersAt);
+  }
 }
