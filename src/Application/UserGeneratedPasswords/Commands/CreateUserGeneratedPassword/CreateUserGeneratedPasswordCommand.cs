cad2fc53fda8dd36a067b150223cf7156a5ac6cb
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-24 23:36:31 +0300

    Add `CreateUserGeneratedPasswordCommand` to decouple API controller from application business logic

---
+++ src/Application/UserGeneratedPasswords/Commands/CreateUserGeneratedPassword/CreateUserGeneratedPasswordCommand.cs
@@ -0,0 +1,51 @@
+﻿using OneTimePassGen.Application.Common.Interfaces;
+using OneTimePassGen.Domain.Entities;
+
+using MediatR;
+using OneTimePassGen.Application.Common.Security;
+
+namespace OneTimePassGen.Application.UserGeneratedPasswords.Commands.CreateUserGeneratedPassword;
+#pragma warning disable MA0048 // File name must match type name
+
+[Authorize]
+public sealed class CreateUserGeneratedPasswordCommand : IRequest<Guid>
+{
+}
+
+internal sealed class CreateUserGeneratedPasswordCommandHandler : IRequestHandler<CreateUserGeneratedPasswordCommand, Guid>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly ICurrentUserService _currentUserService;
+
+    public CreateUserGeneratedPasswordCommandHandler(
+        IApplicationDbContext dbContext,
+        ICurrentUserService currentUserService)
+    {
+        _dbContext = dbContext;
+        _currentUserService = currentUserService;
+    }
+
+    public async Task<Guid> Handle(CreateUserGeneratedPasswordCommand request, CancellationToken cancellationToken = default)
+    {
+        string currentUserId = _currentUserService.UserId ?? throw new UnauthorizedAccessException("User unauthorized to execute this action.");
+        string passwordValue = Guid.NewGuid().ToString();
+        var now = DateTimeOffset.Now;
+
+        // TODO: Move `GeneratedPasswordExpirationSeconds` value to config file.
+        const double generatedPasswordExpirationSeconds = 30;
+        var expiresAt = now.AddSeconds(generatedPasswordExpirationSeconds);
+
+        var generatedPassword = new UserGeneratedPassword
+        {
+            Id = Guid.NewGuid(),
+            UserId = currentUserId,
+            Password = passwordValue,
+            CreatedAt = now,
+            ExpiersAt = expiresAt
+        };
+
+        await _dbContext.UserGeneratedPasswords.AddAsync(generatedPassword, cancellationToken).ConfigureAwait(false);
+        await _dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+        return generatedPassword.Id;
+    }
+}
--- src/Server/Controllers/UserGeneratedPasswordController.cs
+++ src/Server/Controllers/UserGeneratedPasswordController.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-using OneTimePassGen.Application.Common.Interfaces;
+using OneTimePassGen.Application.UserGeneratedPasswords.Commands.CreateUserGeneratedPassword;
 using OneTimePassGen.Application.UserGeneratedPasswords.Models;
 using OneTimePassGen.Application.UserGeneratedPasswords.Queries.GetUserGeneratedPassword;
 using OneTimePassGen.Application.UserGeneratedPasswords.Queries.GetUserGeneratedPasswords;
-using OneTimePassGen.Domain.Entities;
-using OneTimePassGen.Infrastructure.Persistance;
 
 namespace OneTimePassGen.Server.Controllers;
 
@@ -16,15 +14,6 @@ namespace OneTimePassGen.Server.Controllers;
 [Produces("application/json")]
 public sealed class UserGeneratedPasswordController : ApiControllerBase
 {
-    private readonly ApplicationDbContext _dbContext;
-    private readonly ICurrentUserService _currentUserService;
-
-    public UserGeneratedPasswordController(ApplicationDbContext dbContext, ICurrentUserService currentUserService)
-    {
-        _dbContext = dbContext;
-        _currentUserService = currentUserService;
-    }
-
     [HttpGet]
     [ProducesResponseType(typeof(IList<UserGeneratedPasswordItem>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
@@ -58,33 +47,17 @@ public sealed class UserGeneratedPasswordController : ApiControllerBase
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status403Forbidden)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-    public async Task<ActionResult<UserGeneratedPasswordItem>> CreateUserPasswordAsync(CancellationToken cancellationToken = default)
+    public async Task<ActionResult<UserGeneratedPasswordItem>> CreateUserPasswordAsync(
+        CreateUserGeneratedPasswordCommand createCommand,
+        CancellationToken cancellationToken = default)
     {
-        string currentUserId = _currentUserService.UserId ?? throw new UnauthorizedAccessException("User unauthorized to execute this action.");
-        string passwordValue = Guid.NewGuid().ToString();
-        var now = DateTimeOffset.Now;
-        const double generatedPasswordExpirationSeconds = 30;
-        var expiresAt = now.AddSeconds(generatedPasswordExpirationSeconds);
+        var generatedPasswordId = await Mediator.Send(createCommand, cancellationToken);
+        if (generatedPasswordId == Guid.Empty)
+            return NoContent();
 
-        var generatedPassword = new UserGeneratedPassword
-        {
-            Id = Guid.NewGuid(),
-            UserId = currentUserId,
-            Password = passwordValue,
-            CreatedAt = now,
-            ExpiersAt = expiresAt
-        };
+        var getRequest = new GetUserGeneratedPasswordQuery { Id = generatedPasswordId };
+        var userGeneratedPasswordItem = await Mediator.Send(getRequest, cancellationToken);
 
-        await _dbContext.UserGeneratedPasswords.AddAsync(generatedPassword, cancellationToken).ConfigureAwait(false);
-        await _dbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
-
-        var generatedPasswordDto = new UserGeneratedPasswordItem
-        {
-            Id = generatedPassword.Id,
-            Password = generatedPassword.Password,
-            ExpiresAt = generatedPassword.ExpiersAt,
-            CreatedAt = generatedPassword.CreatedAt
-        };
-        return CreatedAtAction(nameof(GetUserPasswordAsync), new { generatedPasswordDto.Id }, generatedPasswordDto);
+        return CreatedAtAction(nameof(GetUserPasswordAsync), new { userGeneratedPasswordItem?.Id }, userGeneratedPasswordItem);
     }
 }
